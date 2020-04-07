354f6b475b25561bb626055768f85f95a378bba7
Mia <mia.mcsweeney@mycit.ite>
2020-04-07 14:57:27 +0100

    Set up Main menu and created another validation function called only_number()

--- Mia_Mc_Sweeney_R00187531_Python_Project.py
+++ Mia_Mc_Sweeney_R00187531_Python_Project.py
@@ -31,6 +31,29 @@ def read_file(name_of_file): #I used this to open the file details and modules a
     connection.close()
     return list1, list2
 
+def get_menu(): #This is used to print the menu
+    print("\nModule Record System - Options")
+    print("-"*30)
+    print("1. Record Attendence\n2. Generate Statistics\n3. Exit")
+
+def only_number(prompt, fixed_number): #This is used to validate a specific input of a number in a certain range
+    fixed_number = fixed_number + 1
+    list1 = []
+    i = 1
+    while i < fixed_number:
+        list1.append(i)
+        i = i + 1
+
+    while True:
+        try:
+            number = int(input(prompt))
+            if number in list1:
+                return number
+            else:
+                print("Please input the correct range")
+        except ValueError:
+            print("Put in a number.")
+
 def main(): #This includes the repetition of the menu and welcomes the user
     while True:
         name = get_details()
